93588b4167ae8ffe5a68ad03616975c7844dbe3a
wats0n <wats0n.edx@gmail.com>
2013-07-10 09:23:27 +0000

    Read index.html

--- web.js
+++ web.js
@@ -1,10 +1,12 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
-
+var fs = require('fs');
+var buffer = new Buffer(fs.readFileSync('./index.html'));
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
-});
+  //response.send('Hello World2!');
+  response.send(buffer.toString());
+));
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
