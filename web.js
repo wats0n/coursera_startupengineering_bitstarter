2f7147d23ad1ee6e6f88999c3157c05d42eab6ed
wats0n <wats0n.edx@gmail.com>
2013-07-10 09:27:12 +0000

    Read index.html

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var buffer = new Buffer(fs.readFileSync('./index.html'));
 app.get('/', function(request, response) {
   //response.send('Hello World2!');
   response.send(buffer.toString());
-));
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
